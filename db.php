905ab2177886dfebdce03a3b61fa77a6671fb028
metchis zainadino mathombe <165237157+Metchismathombe@users.noreply.github.com>
2024-10-29 19:41:57 +0200

    inventory page

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root"; // Remove "@localhost" from the username
-$password = "Password2024";
-$dbname = "csdba01";
+$host = 'localhost';
+$user = 'root';  // Default for XAMPP/WAMP
+$password = 'Met@thombetesting12';  // Leave blank for XAMPP/WAMP
+$dbname = '2024fmsdb01';
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
