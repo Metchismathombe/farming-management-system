1a653a1765980ff3b8e98a80fa0179a35f2f7f4c
metchis zainadino mathombe <165237157+Metchismathombe@users.noreply.github.com>
2024-10-29 19:45:15 +0200

    livestock page

--- db.php
+++ db.php
@@ -1,14 +1,13 @@
 <?php
 $host = 'localhost';
-$user = 'root';  // Default for XAMPP/WAMP
-$password = 'Met@thombetesting12';  // Leave blank for XAMPP/WAMP
-$dbname = '2024fmsdb01';
+$user = 'root';  // Change to your MySQL username
+$password = 'Password@2024';  // Change to your MySQL password
+$dbname = 'PTA4DB01';
 
-// Create connection
-$conn = new mysqli($host, $user, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
 }
 ?>
