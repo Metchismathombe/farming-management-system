d26978380492e18ca76e15e7578d59921e5102da
metchis zainadino mathombe <165237157+Metchismathombe@users.noreply.github.com>
2024-10-29 19:48:39 +0200

    order page

--- db.php
+++ db.php
@@ -1,13 +1,14 @@
 <?php
-$host = 'localhost';
-$user = 'root';  // Change to your MySQL username
-$password = 'Password@2024';  // Change to your MySQL password
-$dbname = 'PTA4DB01';
+$servername = "localhost";
+$username = "root"; // change if different
+$password = ""; // change if set
+$dbname = "farming_management";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Could not connect to the database $dbname :" . $e->getMessage());
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
